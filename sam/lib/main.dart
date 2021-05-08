dfa43a9fc1a4bd4e4fc588ddc407e7f7ca18dd6d
Summaiya Jabeen <summaiyajabeen890@gmail.com>
2021-05-08 14:50:06 +0500

    Assinment 1

---
+++ sam/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text(
+            "I am Summaiya Jabeen: & My Email ID is summaiyajabeen890@gmail.com"),
+      ),
+    );
+  }
+}
